938f06fe1bcb01c1d2e6fef2ed033824242142ba
silasgon <silasidc@gmail.com>
2020-09-17 18:52:19 -0300

    Metodos http, utilisando insomnia, tipos de parametros , aplicação funcional.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const { request, response } = require('express');
+
+const {uuid} = require('uuidv4');
 const app = express();
+
+app.use(express.json());
+
 /**
  * Metodos HTTP:
  * 
@@ -9,32 +14,72 @@ const app = express();
  * PUT/PATCH: Alterar uma informação no back-end
  * DELETE: Deletar uma informação no back-end 
  */
+/**
+ * Tipos de Parâmetros
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Params: Conteúdo na hora de criar ou editar um recurso (JSON)
+*/
+const projects = [];
+
 app.get('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-    ]);
+
+    const {title} = request.query;
+
+ const results = title
+ ? projects.filter(project => project.title.includes(title))
+ : projects;
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        'Projeto 1',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+
+    const{title,owner} = request.body;
+
+    const project = {id: uuid(), title, owner};
+
+    projects.push(project);
+
+    return response.json(project);
 })
+
 app.put('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 4',
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+
+    const {id} = request.params;
+    const{title,owner} = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+     return response.json(project);
 })
+
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        'Projeto 2',
-        'Projeto 3',
-    ]);
+
+    const {id} = request.params;
+
+ const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: 'Project not found.' });
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send();
 })
 app.listen(3333, () => {
     console.log('🚀Back-end Started!');
