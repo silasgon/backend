2f6231a1e284ae0b4394b891caad4030370f6d84
Silas Gon <silasidc@gmail.com>
2020-09-18 01:12:22 -0300

    conceitos iniciais de middleeare.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { request, response } = require('express');
 
-const {uuid} = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 const app = express();
 
 app.use(express.json());
 
-/**
+/** 
  * Metodos HTTP:
  * 
  * GET: Buscar informações do back-end "Servidor".
@@ -14,31 +14,59 @@ app.use(express.json());
  * PUT/PATCH: Alterar uma informação no back-end
  * DELETE: Deletar uma informação no back-end 
  */
+
+
 /**
  * Tipos de Parâmetros
  * 
  * Query Params: Filtros e paginação
  * Route Params: Identificar recursos (Atualizar/Deletar)
  * Request Params: Conteúdo na hora de criar ou editar um recurso (JSON)
-*/
+ */
+
+
+/**
+ * Middleware
+ * Interceptador de requisições que pode interroper totalmente a requisição ou alterar dados da requisição
+ */
 const projects = [];
+
+function logRequests(request, response, next) {
+    const { method, url } = request;
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+    console.log(logLabel);
+
+    return next(); //Proximo middleware
+}
+
+function validateProjectId(request, response, nex) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        return response.status(400).json({ error: 'Invalid prooject ID.' });
+    }
+    return next();
+}
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
 
-    const {title} = request.query;
+    const { title } = request.query;
 
- const results = title
- ? projects.filter(project => project.title.includes(title))
- : projects;
+    const results = title ?
+        projects.filter(project => project.title.includes(title)) :
+        projects;
 
     return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
 
-    const{title,owner} = request.body;
+    const { title, owner } = request.body;
 
-    const project = {id: uuid(), title, owner};
+    const project = { id: uuid(), title, owner };
 
     projects.push(project);
 
@@ -47,8 +75,8 @@ app.post('/projects', (request, response) => {
 
 app.put('/projects/:id', (request, response) => {
 
-    const {id} = request.params;
-    const{title,owner} = request.body;
+    const { id } = request.params;
+    const { title, owner } = request.body;
 
     const projectIndex = projects.findIndex(project => project.id === id);
 
@@ -64,14 +92,14 @@ app.put('/projects/:id', (request, response) => {
 
     projects[projectIndex] = project;
 
-     return response.json(project);
+    return response.json(project);
 })
 
 app.delete('/projects/:id', (request, response) => {
 
-    const {id} = request.params;
+    const { id } = request.params;
 
- const projectIndex = projects.findIndex(project => project.id === id);
+    const projectIndex = projects.findIndex(project => project.id === id);
 
     if (projectIndex < 0) {
         return response.status(400).json({ error: 'Project not found.' });
